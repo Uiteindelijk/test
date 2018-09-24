7189dbf712dc3a9cb28e6b17e9de8b4503656d17
DESKTOP-4TR8F29\Guust <diedriek@gmail.com>
2018-09-24 17:49:20 +0200

    aa
    aaaaa

--- Assets/Scripts/CharacterControllerBehaviour.cs
+++ Assets/Scripts/CharacterControllerBehaviour.cs
@@ -1,13 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
 public class CharacterControllerBehaviour : MonoBehaviour
 {
-
+    [SerializeField]
     private CharacterController _charContrl;
     private Vector3 _velocity = Vector3.zero;
+    private Vector3 _inputMovement = Vector3.zero;
+    private Transform _absoluteTransform;
 
 	void Start ()
     {
@@ -25,18 +28,50 @@ public class CharacterControllerBehaviour : MonoBehaviour
 #endif
 
     }
-	
-	void Update ()
+
+    void Update()
     {
-        if(!_charContrl.isGrounded)
-        _velocity += Physics.gravity * Time.deltaTime;
+        _inputMovement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
 
+    void FixedUpdate ()
+    {
+        GroundGravity();
 
+        ApplyGravity();
+
+        DoMovement();
+        
+	}
+
+    private void ApplyMovement()
+    {
+
+        
+
+        
+    }
+
+    private void DoMovement()
+    {
         Vector3 movement = _velocity * Time.deltaTime;
 
         _charContrl.Move(movement);
+    }
 
+    private void GroundGravity()
+    {
+        if (_charContrl.isGrounded)
+        {
+            _velocity -= Vector3.Project(_velocity, Physics.gravity);
+        }
+    }
 
-	}
-
+    private void ApplyGravity()
+    {
+        if (!_charContrl.isGrounded)
+        {
+            _velocity += Physics.gravity * Time.fixedDeltaTime;
+        }
+    }
 }
