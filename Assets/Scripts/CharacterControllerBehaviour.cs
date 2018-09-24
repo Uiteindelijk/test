4c42e0d2b13036bccbf1df23757b0dd0d78e4489
DESKTOP-4TR8F29\Guust <diedriek@gmail.com>
2018-09-24 16:36:01 +0200

    debug stuff in script gestoke
    wel ldus niet veel gebeurd, gewoon in script charcontrolbehaviour een twuk gestoken van euh alst ti geen charcontrol vind krijg je nun error

--- Assets/Scripts/CharacterControllerBehaviour.cs
+++ Assets/Scripts/CharacterControllerBehaviour.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
-public class CharacterControllerBehaviour : MonoBehaviour {
+public class CharacterControllerBehaviour : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private CharacterController _charContrl;
+
+	void Start ()
+    {
+        _charContrl = GetComponent<CharacterController>();
+
+#if DEBUG
+
+        //if (_charContrl == null)
+        //{
+        //    Debug.LogError("charcontrolbeghaviour needs a charactercontrolcompoment");
+        //}
+
+        Assert.IsNotNull(_charContrl, "uhm test, der is geen charcontroller  pls fix");
+
+#endif
+
+    }
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
 }
